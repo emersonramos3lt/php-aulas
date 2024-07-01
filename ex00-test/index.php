165c58bda62031adeb3d55676486f1a029c3133f
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-07-01 13:59:16 -0300

    Basics

---
+++ ex00-test/index.php
@@ -0,0 +1,43 @@
+<?php 
+    echo"I love pizza <br>";
+    echo"It's really good";
+    // Tudo que for colocado dentro do echo"" será mostrado na página
+    // Podemos usar tags HTML como <br>
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
+
+<!--
+    PARA COLOCAR O CAMINHO NO JSON:
+    
+    1. Abra seus arquivos, e vá até Disco Local
+
+    2. Em Disco Local, click em "xampp", e depois "php"
+
+    3. Role até encontrar "php", clique com o botão esquerdo do mouse, e clique em propriedades, e depois copie o local C:\xampp\php
+-->
+
+<!--
+    PARA VER O CONTEÚDO DA SUA PÁGINA NO PHP:
+
+    1. Vá até Disco Local, acesse xampp
+
+    2. Dentro do xampp, clique na lupa na parte direita de cima, e escreva: htdocs
+
+    3. Clique em htdocs, crie uma pasta para seus projetos, ex: "php"
+
+    4. Coloque seus exércisios dentro da pasta que você criou
+
+    5. Vá no Chrome e escreva o caminho, ex: http://localhost/php/ex00-test/index.php
+-->

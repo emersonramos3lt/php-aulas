6c56eb80b650921051197898e86bb34f685ee25d
Emerson <151971499+emersonramos3lt@users.noreply.github.com>
2024-07-12 13:16:16 -0300

    isset and empty

---
+++ ex13-isset-empty/index.php
@@ -0,0 +1,37 @@
+<?php
+    // isset() = Returns TRUE if a variable is declared and not null
+    // empty() = Returns TRUE if a variable is not declared, false, null, ""
+
+    // TRUE
+    //$username = true;
+    //$username = "Emerson";
+
+    // FALSE 
+    //$username = false;
+    //$username = null;
+
+    /*
+    if(empty($username)) {
+        echo "This variable is empty";
+    } // Sem valor em $username
+
+    else {
+        echo "This variable is NOT empty";
+    } // Com valor $username
+     */
+
+    /*
+    if(isset($username)) {
+        echo "This variable is set";
+    } // Há valor no $username
+
+    else {
+        echo "This variable is NOT set";
+    } // Há null no $username
+    */
+
+    //echo isset($username); // Irá retornar 1, se colocarmos null em $username, não retorna nada
+
+
+    
+?>
